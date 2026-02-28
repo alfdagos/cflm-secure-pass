a7844e4a4fa2fd295de363ff3b9431a34e3c2201
Alfredo D'Agostino <alfredo.dagostino@gmail.com>
2026-02-28 16:28:17 +0100

    Fix formatting of BrowserRouter component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter /*basename="/cflm-secure-pass*/">
+      <BrowserRouter /*basename="/cflm-secure-pass"*/ >
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
