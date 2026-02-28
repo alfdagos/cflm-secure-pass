f6306ff270cb8e1c02dc4df22c75f10041f611eb
Alfredo D'Agostino <alfredo.dagostino@gmail.com>
2026-02-28 16:07:34 +0100

    Uncomment base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  //  base: '/cflm-secure-pass/', // Obbligatorio se non hai un dominio custom
+  base: '/cflm-secure-pass/', // Obbligatorio se non hai un dominio custom
   server: {
     host: "::",
     port: 8080,
